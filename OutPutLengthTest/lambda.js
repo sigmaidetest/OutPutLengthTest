c1d2117aac56942e1fd27806d2022acf94c8c3dc
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-04-27 12:06:25 +0530

    wdw

--- OutPutLengthTest/lambda.js
+++ OutPutLengthTest/lambda.js
@@ -2,6 +2,18 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
-	
+	sns.publish({
+		Message: 'dwd',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:318300609668:MyTopic'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
 	callback(null, 'Successfully executed');
 }
